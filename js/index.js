36b0bd9e2ad2b9f7d37bf7d194e0a3967c1baf0a
bemzj <qiuzijia520@qq.com>
2018-04-02 18:35:27 +0800

    xx

--- js/index.js
+++ js/index.js
@@ -2,4 +2,16 @@ $(function(){
 	$('.playgoing').on('click',function(){
 		swiper.slideNext();
 	});
+	
+	var rStatus = false
+	var run = setInterval(function(){
+		if(rStatus == false)
+		{
+			rStatus = true;
+			$('.page92').find('img').attr('src','img/page93.png');
+		}else{
+			rStatus = false;
+			$('.page92').find('img').attr('src','img/page92.png');
+		}
+	},200);
 });
